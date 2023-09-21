123807a9a722d2d09220afff1a879a543100349b
Heinz Friess <110535964+HeinzFriess@users.noreply.github.com>
2023-09-21 07:27:12 +0200

    added static

--- videoflix/videoflix/settings.py
+++ videoflix/videoflix/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'content.apps.ContentConfig',
     'debug_toolbar',
     'django_rq',
+    'import_export',
 ]
 
 MIDDLEWARE = [
@@ -121,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
