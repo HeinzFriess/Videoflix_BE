bf0988ccc4c918ab6db328f5638527c8ca99999e
Heinz Friess <110535964+HeinzFriess@users.noreply.github.com>
2023-09-21 07:54:55 +0200

    added import export

--- videoflix/content/admin.py
+++ videoflix/content/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 from content.models import Video
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 
 # Register your models here.
 
-admin.site.register(Video)
+class VideoResource(resources.ModelResource):
+    class Meta:
+        model = Video
+#@admin.register(Video, VideoAdmin)
+class VideoAdmin(ImportExportModelAdmin):
+    pass
+
+admin.site.register(Video,VideoAdmin)
