ee9ea959fab7d0a337caac8a80f21645b1996c66
Heinz Friess <friess.heinz@gmx.de>
2023-10-12 20:31:56 +0200

    video endpoint implemented

--- videoflix/content/views.py
+++ videoflix/content/views.py
@@ -3,8 +3,19 @@ from django.core.cache.backends.base import DEFAULT_TIMEOUT
 #from django.views.decorators.cache import cachepage 
 from django.conf import settings
 
+from rest_framework import viewsets
+from content.models import Video
+from videoflix.serializers import VideoSerializer
+
 # Create your views here.
 
 CACHE_TTL = getattr(settings, 'CACHE_TTL', DEFAULT_TIMEOUT)
 
 #Für die view : @cachepage(CACHE_TTL)
+
+class VideoViewSet(viewsets.ModelViewSet):
+    """
+    API Endpoint to the Videos
+    """
+    queryset = Video.objects.all()
+    serializer_class = VideoSerializer
--- videoflix/videoflix/urls.py
+++ videoflix/videoflix/urls.py
@@ -1,13 +1,24 @@
+
 from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 
+#from content.views import VideoViewSet
+from content.views import VideoViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'videos', VideoViewSet)
+
+
 if settings.DEBUG:
     import debug_toolbar
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
     path('__debug__/', include(debug_toolbar.urls)),
     path('django-rq/', include('django_rq.urls')),
+    #path('video/', views.VideoViewSet.as_view(), name='video_view'),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
