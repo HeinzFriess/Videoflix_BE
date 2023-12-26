8593f63e063a770fa3521856300bd2fa3574931d
Heinz Friess <110535964+HeinzFriess@users.noreply.github.com>
2023-12-26 12:22:20 +0100

    bugfix force_str

--- videoflix/users/views.py
+++ videoflix/users/views.py
@@ -10,7 +10,7 @@ from django.contrib.auth.tokens import default_token_generator
 from rest_framework import status,generics
 from django.core.mail import send_mail
 from django.utils.http import urlsafe_base64_encode,urlsafe_base64_decode
-from django.utils.encoding import force_bytes,force_text
+from django.utils.encoding import force_bytes,force_str
 from django.template.loader import render_to_string
 
 from rest_framework.authtoken.views import ObtainAuthToken, APIView
@@ -115,7 +115,7 @@ def register(request):
 
 def activate_account(request, uidb64, token):
     try:
-        uid = force_text(urlsafe_base64_decode(uidb64))
+        uid = force_str(urlsafe_base64_decode(uidb64))
         user = CustomUser.objects.get(pk=uid)
     except (TypeError, ValueError, OverflowError, CustomUser.DoesNotExist):
         user = None
