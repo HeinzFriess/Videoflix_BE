6261248b91ab79d04357f9b850182156ea6d9b5e
Heinz Friess <friess.heinz@gmx.de>
2023-12-27 13:56:59 +0100

    bugfix settings.py

--- videoflix/users/admin.py
+++ videoflix/users/admin.py
@@ -14,6 +14,7 @@ class CustomUserAdmin(admin.ModelAdmin):
             'Personal info',
             {
                 'fields': (
+                    'username',
                     'first_name',
                     'last_name',
                     'email',
