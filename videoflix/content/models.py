6073fcd226b01906df32709f5ede0a934b1a8cad
Heinz Friess <friess.heinz@gmx.de>
2024-01-11 23:06:26 +0100

    bugfix tasks

--- videoflix/content/models.py
+++ videoflix/content/models.py
@@ -6,11 +6,11 @@ class Video(models.Model):
     created_at = models.DateField(default=date.today)
     title = models.CharField(max_length=80)
     description = models.CharField(max_length=500)
-    video_file = models.FileField(upload_to='videos' , blank=True, null=True)
-    video_file360p = models.CharField(max_length=80, blank=True, null=True)
-    video_file720p = models.CharField(max_length=80, blank=True, null=True)
-    video_file1080p = models.CharField(max_length=80, blank=True, null=True)
-    video_fileThumbnail = models.CharField(max_length=80, blank=True, null=True)
+    video_file = models.FileField(upload_to='videos/' , blank=True, null=True)
+    video_file360p = models.FileField(upload_to='videos/' , blank=True, null=True)
+    video_file720p = models.FileField(upload_to='videos/' , blank=True, null=True)
+    video_file1080p = models.FileField(upload_to='videos/' , blank=True, null=True)
+    video_fileThumbnail = models.FileField(upload_to='thumbnails/' , blank=True, null=True)
 
     def __str__(self) -> str:
         return self.title 
--- videoflix/content/signals.py
+++ videoflix/content/signals.py
@@ -8,38 +8,38 @@ from django.db.models.signals import post_save, pre_delete
 
 @receiver(post_save, sender=Video)
 def video_post_save(sender, instance, created, **kwargs):
-    # Get the path to the Video file
-    Video_file_path = instance.video_file.path
-
     if created:
         queue = django_rq.get_queue('default',autocommit=True)
-        queue.enqueue(convert360p, Video_file_path)
-        instance.video_file360p = Video_file_path.replace(".mp4", "_360p.mp4")
-        queue.enqueue(convert720p, Video_file_path)
-        instance.video_file720p = Video_file_path.replace(".mp4", "_720p.mp4")
-        queue.enqueue(convert1080p, Video_file_path)
-        instance.video_file1080p = Video_file_path.replace(".mp4", "_1080p.mp4")
-        queue.enqueue(createThumbnail, Video_file_path)
-        instance.video_fileThumbnail = Video_file_path.replace(".mp4", ".jpg")
-    
-    instance.save()
+        # Get the path to the Video file
+        video_file_path = instance.video_file.path
+
+        instance.video_file360p.name = video_file_path.replace(".mp4", "_360p.mp4")
+        queue.enqueue(convert360p, video_file_path, instance.video_file360p.path)
+
+        instance.video_file720p.name = video_file_path.replace(".mp4", "_720p.mp4")
+        queue.enqueue(convert720p, video_file_path, instance.video_file720p.path)
+
+        instance.video_file1080p.name = video_file_path.replace(".mp4", "_1080p.mp4")
+        queue.enqueue(convert1080p, video_file_path, instance.video_file1080p.path)
+
+        instance.video_fileThumbnail.name = video_file_path.replace(".mp4", ".jpg")
+        queue.enqueue(createThumbnail, video_file_path, instance.video_fileThumbnail.path)
+
+        instance.save()
 
 
 @receiver(pre_delete, sender=Video)
 def video_pre_delete(sender, instance, using, **kwargs):
     
     # Check if the file exists and delete it
-    if os.path.isfile(instance.video_file.path):
-        os.remove(instance.video_file.path)
+    if os.path.isfile(instance.video_file1080p.path):
+        os.remove(instance.video_file1080p.path)
 
-    if os.path.isfile(instance.video_file1080p):
-        os.remove(instance.video_file1080p)
+    if os.path.isfile(instance.video_file720p.path):
+        os.remove(instance.video_file720p.path)
 
-    if os.path.isfile(instance.video_file720p):
-        os.remove(instance.video_file720p)
+    if os.path.isfile(instance.video_file360p.path):
+        os.remove(instance.video_file360p.path)
 
-    if os.path.isfile(instance.video_file360p):
-        os.remove(instance.video_file360p)
-
-    if os.path.isfile(instance.video_fileThumbnail):
-        os.remove(instance.video_fileThumbnail)
+    if os.path.isfile(instance.video_fileThumbnail.path):
+        os.remove(instance.video_fileThumbnail.path)
