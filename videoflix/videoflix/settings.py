c3f1e947c8acc40ae8529b4f67d1e891eed2743b
Heinz Friess <110535964+HeinzFriess@users.noreply.github.com>
2024-01-11 14:33:16 +0100

    removed corsehraders

--- videoflix/videoflix/settings.py
+++ videoflix/videoflix/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 import os
 from pathlib import Path
-import corsheaders
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,12 +75,10 @@ INSTALLED_APPS = [
     'import_export',
     'content',
     'users',
-    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
