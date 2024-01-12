684b0d9853e35b1e8378cf4cf6c1667ba43b2410
Heinz Friess <friess.heinz@gmx.de>
2024-01-12 16:41:42 +0100

    added corseheaders again

--- videoflix/videoflix/settings.py
+++ videoflix/videoflix/settings.py
@@ -75,11 +75,13 @@ INSTALLED_APPS = [
     'import_export',
     'content',
     'users',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
