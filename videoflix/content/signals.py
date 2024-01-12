43b8bd5beeca90643455212cd5b9b0cc58af5161
Heinz Friess <friess.heinz@gmx.de>
2024-01-12 14:14:32 +0100

    changed upload tasks

--- videoflix/content/signals.py
+++ videoflix/content/signals.py
@@ -12,18 +12,23 @@ def video_post_save(sender, instance, created, **kwargs):
         queue = django_rq.get_queue('default',autocommit=True)
         # Get the path to the Video file
         video_file_path = instance.video_file.path
-
-        instance.video_file360p.name = video_file_path.replace(".mp4", "_360p.mp4")
-        queue.enqueue(convert360p, video_file_path, instance.video_file360p.path)
-
-        instance.video_file720p.name = video_file_path.replace(".mp4", "_720p.mp4")
-        queue.enqueue(convert720p, video_file_path, instance.video_file720p.path)
-
-        instance.video_file1080p.name = video_file_path.replace(".mp4", "_1080p.mp4")
-        queue.enqueue(convert1080p, video_file_path, instance.video_file1080p.path)
+        video_file_name = instance.video_file.name
 
         instance.video_fileThumbnail.name = video_file_path.replace(".mp4", ".jpg")
         queue.enqueue(createThumbnail, video_file_path, instance.video_fileThumbnail.path)
+        instance.video_fileThumbnail.name = video_file_name.replace(".mp4", ".jpg")
+        
+        instance.video_file360p.name = video_file_path.replace(".mp4", "_360p.mp4")
+        queue.enqueue(convert360p, video_file_path, instance.video_file360p.path)
+        instance.video_file360p.name = video_file_name.replace(".mp4", "_360p.mp4")
+
+        instance.video_file720p.name = video_file_path.replace(".mp4", "_720p.mp4")
+        queue.enqueue(convert720p, video_file_path, instance.video_file720p.path)
+        instance.video_file720p.name = video_file_name.replace(".mp4", "_720p.mp4")
+
+        instance.video_file1080p.name = video_file_path.replace(".mp4", "_1080p.mp4")
+        queue.enqueue(convert1080p, video_file_path, instance.video_file1080p.path)
+        instance.video_file1080p.name = video_file_name.replace(".mp4", "_1080p.mp4")
 
         instance.save()
 
@@ -40,6 +45,9 @@ def video_pre_delete(sender, instance, using, **kwargs):
 
     if os.path.isfile(instance.video_file360p.path):
         os.remove(instance.video_file360p.path)
+        
+    if os.path.isfile(instance.video_file.path):
+        os.remove(instance.video_file.path)
 
     if os.path.isfile(instance.video_fileThumbnail.path):
         os.remove(instance.video_fileThumbnail.path)
